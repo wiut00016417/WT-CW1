a8d3e9ad36048501c59241f06e88f00b641d055e
00016417 <wiut00016417@gmail.com>
2023-11-23 18:55:36 +0500

    added photos & updated code

---
+++ script.js
@@ -0,0 +1,7 @@
+function showCategories () {
+    var x = document.getElementById("layer")
+    if (x.style.display == "block") {
+        x.style.display = "none"
+    } else {x.style.display = "block"}
+}
+
